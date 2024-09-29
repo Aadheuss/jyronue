f0d3c86ac3797b7f85be01662505cb96ac04f6c9
Aadheuss <aadheusx@gmail.com>
2024-09-29 20:00:06 +0700

    Add function to check for chrome autofilled input

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -4,4 +4,15 @@ const isInputEmpty = (e: React.FormEvent<HTMLInputElement>): boolean => {
   return element.value.length > 0;
 };
 
-export { isInputEmpty };
+const checkAutofilled = (
+  inputRef: React.MutableRefObject<null | HTMLInputElement>,
+  setIsAutofilled: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  const current = inputRef.current;
+  if (current) {
+    const autofilled = current.matches("*:-webkit-autofill");
+    setIsAutofilled(autofilled);
+  }
+};
+
+export { isInputEmpty, checkAutofilled };
