173bdf2b073515419d8526bce298ba6f1e4627a5
Aadheuss <aadheusx@gmail.com>
2024-09-30 22:33:09 +0700

    Add a function to fix autofill problem
    Create a function to check input value and mimic client interaction by
    setting its value. this is due to input not registerng autofill after
    user going back to the login page by clicking back button on login error
    page. This causes a bug where input.value is not empty but the input
    acts as if it is empty. focusing the input doesn't seem to register the
    input and the only way for the input to register its value is by
    triggering input change.

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -7,7 +7,7 @@ const isInputEmpty = (e: React.FormEvent<HTMLInputElement>): boolean => {
 const checkAutofilled = (
   inputRef: React.MutableRefObject<null | HTMLInputElement>,
   setIsAutofilled: React.Dispatch<React.SetStateAction<boolean>>
-) => {
+): void => {
   const current = inputRef.current;
   if (current) {
     const autofilled = current.matches("*:-webkit-autofill");
@@ -15,4 +15,22 @@ const checkAutofilled = (
   }
 };
 
-export { isInputEmpty, checkAutofilled };
+const registerAutofilledInput = (
+  inputRef: React.MutableRefObject<null | HTMLInputElement>
+): void => {
+  // Fix autofill input not registering on input
+  const current = inputRef.current;
+  if (current) {
+    const value = current.value;
+    // Only fix non empty value
+    // input value causes sync problem on chrome when using useNavigate(-1)
+    // focusing on the input also doesn't seem to cause the input to be detected
+    // non empty input value seems to not be registered until the user starting changing the input value
+
+    if (value) {
+      current.value = value;
+    }
+  }
+};
+
+export { isInputEmpty, checkAutofilled, registerAutofilledInput };
