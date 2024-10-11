b86551d945d8be83b93e3fe041bbae87af75e8ce
Aadheuss <aadheusx@gmail.com>
2024-10-11 11:11:05 +0700

    Unescape comment content and change its style css

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./CommentList.module.css";
 import { Link, useParams } from "react-router-dom";
+import { unescapeInput } from "../../utils/htmlDecoder";
 
 type CommentValue = {
   id: string;
@@ -61,43 +62,47 @@ const CommentList = () => {
   return (
     <ul className={styles.comments}>
       <p className={styles.text}>Comments</p>
-      {comments && comments.length > 0 ? (
-        comments.map((comment) => {
-          return (
-            <li className={styles.comment} key={comment.id}>
-              <Link to={`/profile/${comment.author.username}`}>
-                <img
-                  className={styles.commentUserProfile}
-                  src={comment.author.profileImage.pictureUrl}
-                ></img>
-              </Link>
+      <div className={styles.commentList}>
+        {comments && comments.length > 0 ? (
+          comments.map((comment) => {
+            return (
+              <li className={styles.comment} key={comment.id}>
+                <Link to={`/profile/${comment.author.username}`}>
+                  <img
+                    className={styles.commentUserProfile}
+                    src={comment.author.profileImage.pictureUrl}
+                  ></img>
+                </Link>
 
-              <div className={styles.mainComment}>
-                <div className={styles.commentProfileInfo}>
-                  <Link
-                    to={`/profile/${comment.author.username}`}
-                    className={styles.commentDisplayName}
-                  >
-                    {comment.author.displayName}
-                  </Link>
+                <div className={styles.mainComment}>
+                  <div className={styles.commentProfileInfo}>
+                    <Link
+                      to={`/profile/${comment.author.username}`}
+                      className={styles.commentDisplayName}
+                    >
+                      {comment.author.displayName}
+                    </Link>
 
-                  <Link
-                    to={`/profile/${comment.author.username}`}
-                    className={styles.commentUsername}
-                  >
-                    {`@${comment.author.username}`}
-                  </Link>
+                    <Link
+                      to={`/profile/${comment.author.username}`}
+                      className={styles.commentUsername}
+                    >
+                      {`@${comment.author.username}`}
+                    </Link>
+                  </div>
+                  <p className={styles.commentText}>
+                    {unescapeInput(comment.content)}
+                  </p>
                 </div>
-                <p className={styles.commentText}>{comment.content}</p>
-              </div>
-            </li>
-          );
-        })
-      ) : (
-        <li>
-          <p>No comments yet</p>
-        </li>
-      )}
+              </li>
+            );
+          })
+        ) : (
+          <li className={styles.comment}>
+            <p className={styles.commentText}>No comments yet</p>
+          </li>
+        )}
+      </div>
     </ul>
   );
 };
