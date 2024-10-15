ed46d4eaa0834592a3af48b1963e2a4df0c584b2
Aadheuss <aadheusx@gmail.com>
2024-10-15 10:57:15 +0700

    Display different buttons on NavBar
    Display different buttons on nav bar based on user
    context.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,62 @@
 import styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/jyronue-logo.svg";
+import { useContext } from "react";
+import { UserContext } from "../../context/context";
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const logout = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/user/logout", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      const resData = await res.json();
+      console.log(resData);
+
+      if (resData.error) {
+        console.log(resData.error);
+      } else {
+        setUser(null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <nav className={styles.navBar}>
       <Link className={styles.link} to="/">
         <img className={styles.logo} src={logo} alt="Jyronue-logo" />
       </Link>
       <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to="/login">
-            Log in
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to="/signup">
-            Sign up
-          </Link>
-        </li>
+        {user ? (
+          <li className={styles.navItem}>
+            <button
+              className={styles.logoutButton}
+              type="button"
+              onClick={logout}
+            >
+              Log out
+            </button>
+          </li>
+        ) : (
+          <>
+            <li className={styles.navItem}>
+              <Link className={styles.link} to="/login">
+                Log in
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link className={styles.link} to="/signup">
+                Sign up
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
