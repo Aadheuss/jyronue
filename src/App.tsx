57bcb477a81858be4f9020c4a673a32e43c864b3
Aadheuss <aadheusx@gmail.com>
2024-10-15 10:50:47 +0700

    Implement user context on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import { getUser } from "./utils/authentication";
+import { useState, useEffect } from "react";
+import { UserContext } from "./context/context";
+
+type User = {
+  id: string;
+};
 
 function App() {
+  const [user, setUser] = useState<null | User>(null);
+
+  useEffect(() => {
+    const configureUserState = async () => {
+      const user = await getUser();
+
+      if (user) {
+        setUser(user);
+      }
+    };
+
+    configureUserState();
+  }, []);
+
   return (
     <>
-      <Outlet />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Outlet />
+      </UserContext.Provider>
     </>
   );
 }
