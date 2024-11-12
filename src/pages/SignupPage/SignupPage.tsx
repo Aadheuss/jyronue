25d21cf8477e7b25c2b5095cfdd2aeca00fce7de
Aadheuss <aadheusx@gmail.com>
2024-11-12 17:36:11 +0700

    Implement responsive design on signup page

--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -49,106 +49,109 @@ const SignupPage = () => {
 
   return (
     <div className={styles.signupPageWrapper}>
-      <main className={styles.main}>
-        <div className={styles.imgContainer}>
-          <img className={styles.logo} src={logo} alt="Jyronue-logo" />
-        </div>
-        <div className={styles.mainItem}>
-          <div className={styles.greeting}>
-            <h1 className={styles.h1}>Welcome to Jyronue</h1>
-            <p className={styles.text}>Create an account to get started</p>
+      <div className={styles.mainWrapper}>
+        <main className={styles.main}>
+          <div></div>
+          <div className={styles.imgContainer}>
+            <img className={styles.logo} src={logo} alt="Jyronue-logo" />
           </div>
-          <FormProvider {...methods}>
-            <form
-              className={styles.signupForm}
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <div className={styles.itemContainer}>
-                {errors.username && (
-                  <span className={styles.errorTxt}>
-                    {errors.username.message}
-                  </span>
-                )}
-                {errorList &&
-                  errorList.map(
-                    (error: errorValue, index) =>
-                      error.field === "username" && (
-                        <span key={index} className={styles.errorTxt}>
-                          {error.msg}
-                        </span>
-                      )
+          <div className={styles.mainItem}>
+            <div className={styles.greeting}>
+              <h1 className={styles.h1}>Welcome to Jyronue</h1>
+              <p className={styles.text}>Create an account to get started</p>
+            </div>
+            <FormProvider {...methods}>
+              <form
+                className={styles.signupForm}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <div className={styles.itemContainer}>
+                  {errors.username && (
+                    <span className={styles.errorTxt}>
+                      {errors.username.message}
+                    </span>
                   )}
-                <InputContainer
-                  form={{
-                    focus,
-                    setFocus,
-                    setErrorList,
-                  }}
-                  labelType="HIDDEN"
-                  id="username"
-                  autoComplete="username"
-                  label="Username"
-                  validation={{
-                    required: "Username is required",
-                    maxLength: {
-                      value: 32,
-                      message: "Username cannot exceed 32 characters",
-                    },
-                    pattern: {
-                      value: /^[a-zA-Z0-9_]+$/,
-                      message:
-                        "Username must only contain letters, numbers and underscores",
-                    },
-                  }}
-                />
-              </div>
+                  {errorList &&
+                    errorList.map(
+                      (error: errorValue, index) =>
+                        error.field === "username" && (
+                          <span key={index} className={styles.errorTxt}>
+                            {error.msg}
+                          </span>
+                        )
+                    )}
+                  <InputContainer
+                    form={{
+                      focus,
+                      setFocus,
+                      setErrorList,
+                    }}
+                    labelType="HIDDEN"
+                    id="username"
+                    autoComplete="username"
+                    label="Username"
+                    validation={{
+                      required: "Username is required",
+                      maxLength: {
+                        value: 32,
+                        message: "Username cannot exceed 32 characters",
+                      },
+                      pattern: {
+                        value: /^[a-zA-Z0-9_]+$/,
+                        message:
+                          "Username must only contain letters, numbers and underscores",
+                      },
+                    }}
+                  />
+                </div>
 
-              <div className={styles.itemContainer}>
-                {errors.password && (
-                  <span className={styles.errorTxt}>
-                    {errors.password.message}
-                  </span>
-                )}
-                {errorList &&
-                  errorList.map(
-                    (error: errorValue, index) =>
-                      error.field === "password" && (
-                        <span key={index} className={styles.errorTxt}>
-                          {error.msg}
-                        </span>
-                      )
+                <div className={styles.itemContainer}>
+                  {errors.password && (
+                    <span className={styles.errorTxt}>
+                      {errors.password.message}
+                    </span>
                   )}
-                <InputContainer
-                  form={{
-                    focus,
-                    setFocus,
-                    setErrorList,
-                  }}
-                  labelType="HIDDEN"
-                  id="password"
-                  type="password"
-                  autoComplete="new-password"
-                  label="Password"
-                  validation={{
-                    required: "Password is required",
-                    minLength: {
-                      value: 8,
-                      message: "Password must be longer than 8 characters",
-                    },
-                    pattern: {
-                      value: /^\S[\s\S]+\S$/,
-                      message:
-                        "Password cannot start or end with white space characters",
-                    },
-                  }}
-                />
-              </div>
+                  {errorList &&
+                    errorList.map(
+                      (error: errorValue, index) =>
+                        error.field === "password" && (
+                          <span key={index} className={styles.errorTxt}>
+                            {error.msg}
+                          </span>
+                        )
+                    )}
+                  <InputContainer
+                    form={{
+                      focus,
+                      setFocus,
+                      setErrorList,
+                    }}
+                    labelType="HIDDEN"
+                    id="password"
+                    type="password"
+                    autoComplete="new-password"
+                    label="Password"
+                    validation={{
+                      required: "Password is required",
+                      minLength: {
+                        value: 8,
+                        message: "Password must be longer than 8 characters",
+                      },
+                      pattern: {
+                        value: /^\S[\s\S]+\S$/,
+                        message:
+                          "Password cannot start or end with white space characters",
+                      },
+                    }}
+                  />
+                </div>
 
-              <button type="submit">Sign up</button>
-            </form>
-          </FormProvider>
-        </div>
-      </main>
+                <button type="submit">Sign up</button>
+              </form>
+            </FormProvider>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
