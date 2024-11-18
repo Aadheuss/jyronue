a8db9ac1b0069909da19dabffb4e963df912a564
Aadheuss <aadheusx@gmail.com>
2024-11-18 14:35:43 +0700

    Add spinner on postImages PostDetailsPageSkeleton

--- src/pages/PostDetailsPage/PostDetailsPageSkeleton.tsx
+++ src/pages/PostDetailsPage/PostDetailsPageSkeleton.tsx
@@ -1,9 +1,16 @@
+import Loader from "../../components/Loader/Loader";
 import styles from "./PostDetailsPage.module.css";
 
 const PostDetailsPageSkeleton = () => {
   return (
     <>
-      <div className={styles.postImages}></div>
+      <div className={styles.postImages}>
+        <Loader
+          size={{ width: "2.5em", height: "2.5em" }}
+          color="var(--accent-color-1)"
+          type="spinner"
+        />
+      </div>
 
       <div className={styles.postData}>
         <div className={styles.postDataItem}>
