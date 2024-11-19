baf4a3ffeef755988d32fd5848434d6fbf53825e
Aadheuss <aadheusx@gmail.com>
2024-11-19 15:34:03 +0700

    Implement infinite scroll on CommentList

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useContext, useEffect, useState } from "react";
+import React, { FC, useContext, useEffect, useRef, useState } from "react";
 import styles from "./CommentList.module.css";
 import { useParams } from "react-router-dom";
 import CommentBox from "../CommentBox/CommentBox";
 import { CommentValue } from "../../config/typeValues";
 import Comment from "../Comment/Comment";
 import { UserContext } from "../../context/context";
+import { fetchData } from "../../utils/fetchFunctions";
+import Loader from "../Loader/Loader";
 
 interface Props {
   commentInputRef: React.MutableRefObject<null | HTMLInputElement>;
@@ -15,32 +17,75 @@ const CommentList: FC<Props> = ({ commentInputRef }) => {
   const { postid } = useParams();
   const [comments, setComments] = useState<CommentValue[]>([]);
   const [openReplyId, setOpenReplyId] = useState<string>("");
+  const [cursor, setCursor] = useState<null | false | string>(null);
+  const [isScrollLoading, setIsScrollLoading] = useState<boolean>(false);
+  const observerRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const res = await fetch(
-          `http://localhost:3000/post/${postid}/comments?limit=100`,
-          {
+    const fetchComments = async ({
+      cursor,
+    }: {
+      cursor: null | false | string;
+    }) => {
+      const cursorQuery = cursor ? `&cursor=${cursor}` : "";
+
+      setIsScrollLoading(true);
+
+      if (cursor === null || cursor) {
+        const commentsData = await fetchData({
+          link: `http://localhost:3000/post/${postid}/comments?limit=100${cursorQuery}`,
+          options: {
             method: "GET",
             credentials: "include",
-          }
-        );
+          },
+        });
 
-        const resData = await res.json();
-
-        if (resData.error) {
-          console.log(resData.error);
+        if (commentsData?.isError) {
+          console.error(commentsData?.data.error, commentsData?.data.errors);
         } else {
-          console.log(resData);
-          setComments(resData.comments);
+          setCursor(commentsData?.data.nextCursor);
+          setComments(
+            comments
+              ? [...comments, ...(commentsData?.data.comments || [])]
+              : commentsData?.data.comments
+          );
         }
-      } catch (err) {
-        console.log(err);
       }
+
+      setIsScrollLoading(false);
     };
 
-    fetchComments();
-  }, [postid]);
+    const current = observerRef.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log(entries[0].intersectionRatio);
+        if (entries[0].isIntersecting) {
+          // Fetch new data if only another fetch hasn't started
+          if (!isScrollLoading) {
+            fetchComments({ cursor });
+          }
+        }
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    if (current) {
+      observer.observe(current);
+    }
+
+    if (cursor === null) {
+      // Fetch initial comments
+      fetchComments({ cursor });
+    }
+
+    return () => {
+      if (current) {
+        observer.unobserve(current);
+      }
+    };
+  }, [postid, cursor, comments]);
 
   const updateLikesBox = ({
     likesBox,
@@ -95,18 +140,30 @@ const CommentList: FC<Props> = ({ commentInputRef }) => {
       <p className={styles.commentHeading}>Comments</p>
       <ul className={styles.commentList}>
         {comments && comments.length > 0 ? (
-          comments.map((comment) => {
-            return (
-              <Comment
-                key={comment.id}
-                comment={comment}
-                updateLikesBox={updateLikesBox}
-                openReplyId={openReplyId}
-                setOpenReplyId={setOpenReplyId}
-                updateComment={updateComment}
-              />
-            );
-          })
+          <>
+            {comments.map((comment) => {
+              return (
+                <Comment
+                  key={comment.id}
+                  comment={comment}
+                  updateLikesBox={updateLikesBox}
+                  openReplyId={openReplyId}
+                  setOpenReplyId={setOpenReplyId}
+                  updateComment={updateComment}
+                />
+              );
+            })}
+            {isScrollLoading && (
+              <div className={styles.loaderContainer}>
+                <Loader
+                  type="spinner"
+                  size={{ width: "1.5em", height: "1.5em" }}
+                  color="var(--accent-color-1)"
+                />
+              </div>
+            )}
+            <div ref={observerRef} className={styles.observer}></div>
+          </>
         ) : (
           <li className={styles.comment} key="no-comment-list">
             <p className={styles.commentText}>No comments yet</p>
