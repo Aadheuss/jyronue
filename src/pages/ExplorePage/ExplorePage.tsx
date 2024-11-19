1b45400809ea6b873f6fe6f9e2ba55edf0a3d38c
Aadheuss <aadheusx@gmail.com>
2024-11-19 14:51:26 +0700

    Implement infinite scrolling on explore page

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,39 +1,80 @@
 import styles from "./ExplorePage.module.css";
 import NavBar from "../../components/NavBar/NavBar";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PostGallery } from "../../config/typeValues";
 import { fetchData } from "../../utils/fetchFunctions";
 import Gallery from "../../components/Gallery/Gallery";
 import GallerySkeleton from "../../components/Gallery/GallerySkeleton";
+import Loader from "../../components/Loader/Loader";
 
 const ExplorePage = () => {
   const [posts, setPosts] = useState<null | PostGallery[]>(null);
-  const [isCursor, setIsCursor] = useState<boolean>(true);
+  const [cursor, setCursor] = useState<null | false | string>(null);
+  const observerRef = useRef<null | HTMLDivElement>(null);
+  const [isScrollLoading, setIsScrollLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const postsData = await fetchData({
-        link: "http://localhost:3000/posts",
-        options: {
-          method: "GET",
-          credentials: "include",
-        },
-      });
+    const fetchPosts = async ({
+      cursor,
+    }: {
+      cursor: null | false | string;
+    }) => {
+      const currentQuery = cursor ? `&cursor=${cursor}` : "";
 
-      if (postsData?.isError) {
-        console.log(postsData?.data.error, postsData?.data.errors);
-      } else {
-        setIsCursor(postsData?.data.nextCursor);
-        setPosts(postsData?.data.posts);
+      if (cursor) {
+        setIsScrollLoading(true);
       }
+
+      if (cursor === null || cursor) {
+        const postsData = await fetchData({
+          link: `http://localhost:3000/posts?limit=20${currentQuery}`,
+          options: {
+            method: "GET",
+            credentials: "include",
+          },
+        });
+
+        if (postsData?.isError) {
+          console.log(postsData?.data.error, postsData?.data.errors);
+        } else {
+          setCursor(postsData?.data.nextCursor);
+          setPosts(
+            posts
+              ? [...posts, ...(postsData?.data.posts || [])]
+              : postsData?.data.posts
+          );
+        }
+      }
+
+      setIsScrollLoading(false);
     };
 
-    fetchPosts();
+    const current = observerRef.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchPosts({ cursor });
+        }
+      },
+      {
+        threshold: 1,
+      }
+    );
+
+    if (current) {
+      observer.observe(current);
+    }
+
+    if (cursor === null) {
+      fetchPosts({ cursor });
+    }
 
     return () => {
-      setPosts(null);
+      if (current) {
+        observer.unobserve(current);
+      }
     };
-  }, []);
+  }, [posts, cursor]);
 
   return (
     <>
@@ -42,7 +83,17 @@ const ExplorePage = () => {
         <div className={styles.mainContent}>
           <h2 className={styles.heading}>Explore latest posts</h2>
           {posts ? <Gallery posts={posts} /> : <GallerySkeleton amount={20} />}
-          {!isCursor && (
+          {isScrollLoading && (
+            <div className={styles.loaderContainer}>
+              <Loader
+                type="spinner"
+                size={{ width: "1.5em", height: "1.5em" }}
+                color="var(--accent-color-1)"
+              />
+            </div>
+          )}
+          <div ref={observerRef} className={styles.observer}></div>
+          {cursor === false && (
             <p className={styles.text}>
               You have reached the end, no more posts to explore
             </p>
