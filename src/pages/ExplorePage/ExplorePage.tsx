1ab2eeb87721fe7b8cbcfa2c3441778e2060d05d
Aadheuss <aadheusx@gmail.com>
2024-11-30 18:17:29 +0700

    Handle new error response changes on ExplorePage
    - Handle the new backend error response
    - Use fetch instead of fetchData function
    - log validation errors

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -2,7 +2,6 @@ import styles from "./ExplorePage.module.css";
 import NavBar from "../../components/NavBar/NavBar";
 import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { PostGallery } from "../../config/typeValues";
-import { fetchData } from "../../utils/fetchFunctions";
 import Gallery from "../../components/Gallery/Gallery";
 import GallerySkeleton from "../../components/Gallery/GallerySkeleton";
 import Loader from "../../components/Loader/Loader";
@@ -22,35 +21,42 @@ const ExplorePage = () => {
       const currentQuery = cursor ? `&cursor=${cursor}` : "";
       const limit = 3;
 
+      // Only fetch if the cursor is not false
       if (cursor === null || cursor) {
         if (cursor !== null) {
           setIsScrollLoading(true);
         }
 
         try {
-          const postsData = await fetchData({
-            link: `${domain}/posts?limit=${limit}${currentQuery}`,
-            options: {
+          const postsResponse = await fetch(
+            `${domain}/posts?limit=${limit}${currentQuery}`,
+            {
               mode: "cors",
               method: "GET",
               credentials: "include",
-            },
-          });
+            }
+          );
 
-          if (postsData?.isError) {
-            console.log("Failed to fetch explore page posts");
-            console.log(postsData?.data.error, postsData?.data.errors);
+          const postsData = await postsResponse.json();
+
+          if (postsData.error) {
+            console.log(`${postsData.error.message}: Explore page posts`);
+            // Log validation error
+            if (postsData.error.errors) {
+              const errorList = postsData.error.errors;
+
+              errorList.forEach(
+                (error: { field: string; value: string; msg: string }) =>
+                  console.log(error.msg)
+              );
+            }
           } else {
             console.log("Successfully fetched explore page posts");
             const nextCursor =
-              postsData?.data.posts.length >= limit
-                ? postsData?.data.nextCursor
-                : false;
+              postsData.posts.length >= limit ? postsData.nextCursor : false;
             setCursor(nextCursor);
             setPosts(
-              posts
-                ? [...posts, ...(postsData?.data.posts || [])]
-                : postsData?.data.posts
+              posts ? [...posts, ...(postsData.posts || [])] : postsData.posts
             );
           }
         } catch (err) {
